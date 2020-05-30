728df1a50c565654dc63c5519f62781ddbf4defb
Asha Ivey <ashavivey@gmail.com>
2020-05-29 22:27:20 -0500

    Add content, images, and javascript functionality to the front end of the portfolio webpage.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,12 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+var diamondHitCount = 0;
+
+
 /**
  * Adds a random greeting to the page.
  */
+
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!',
+       'こんにちは,世界', 'Привет мир'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
@@ -25,4 +30,66 @@ function addRandomGreeting() {
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
+}
+
+/**
+* Change height of diamond on page by randomly changing the diamond's top margin.
+*/
+function moveDiamondOnclick() {
+    const diamond = document.getElementById('diamond');
+    var heightMax = document.getElementById('trifecta').offsetHeight;
+    heightMax = heightMax - diamond.offsetHeight;
+
+    var changeTo = Math.floor(Math.random() * heightMax);
+
+    diamond.style.paddingTop = changeTo + "px";
+    diamondHitCount = diamondHitCount + 1;
+    
+    diamondDescriptionUpdate(diamondHitCount);
+}
+
+/**
+* Change the description to show how many times the diamond has been pressed, 
+* along with a little message.
+*/
+function diamondDescriptionUpdate(hitCount) {
+    const diaGame = document.getElementById('diaGame');
+    const description = document.getElementById('diaDescription');
+
+    diaGame.innerText = "Hit the Diamond!";
+
+    if (hitCount == 1) {
+        description.innerText = "You've hit the Diamond " + hitCount + 
+        " time! This means nothing..."
+
+    } else if (hitCount <= 5) {
+        description.innerText = "You've hit the Diamond " + hitCount + 
+        " times! This means nothing..."
+
+    } else if (hitCount <= 10) {
+        description.innerText = "You've hit the Diamond " + hitCount + 
+        " times! This really means nothing. Please stop."
+
+    } else if (hitCount <= 15) {
+        description.innerText = "You've hit the Diamond " + hitCount + 
+        " times! Do what you want, I guess, but it is a waste of time..."
+
+    } else if (hitCount <= 20 ) {
+        description.innerText = "You've hit the Diamond " + hitCount + 
+        " times! What? But why tho?"
+
+    } else if (hitCount <= 30 ) {
+        description.innerText = "You've hit the Diamond " + hitCount + 
+        " times! I have no words, I-"
+
+    } else if (hitCount <= 100 ) {
+        description.innerText = "You've hit the Diamond " + hitCount + 
+        " times! Please spend your time NOT hitting the diamond, okay?"
+        
+    } else {
+        description.innerText = "You've hit the Diamond " + hitCount + 
+        " times! There is something wrong with you. You did this for what?" +
+        " Please, and I mean PLEASE, stop hitting the diamond. You have" +
+        " nothing more to gain from this."
+    }
 }
